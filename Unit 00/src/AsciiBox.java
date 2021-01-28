910d55c4b20683064d8862dc8d454d13174cf638
leeb5737 <58750877+leeb5737@users.noreply.github.com>
2021-01-28 13:34:06 -0800

    Unit 1 push and fixed problems

--- Unit 00/src/AsciiBox.java
+++ Unit 00/src/AsciiBox.java
@@ -10,7 +10,7 @@ public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
+		System.out.println("Bryton L. \t  29 January 2021 \n\n" );
 		System.out.println("+++++++++++++++++++++++++ " );
 
 	}

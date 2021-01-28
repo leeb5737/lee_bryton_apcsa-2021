0039de34ea9c3c0e49c23d79462fe934e0a5b5ce
leeb5737 <58750877+leeb5737@users.noreply.github.com>
2021-01-28 13:22:11 -0800

    0a 3 Finished

--- Unit 00/src/AsciiArt.java
+++ Unit 00/src/AsciiArt.java
@@ -1,28 +1,38 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
+//Name - Bryton L.
+//Date - 28 January 2021
+//Class - AP Computer Science A 
 //Lab  -
 
 public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
-		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("Bryton Lee       28 January 2021 \n\n" );
+		System.out.println("Here is my animal drawing:" );
 		System.out.println("\n\n\n\n" );
 
-		System.out.println("                /\\                " );
-		System.out.println("               /  \\               " );
-		System.out.println("              /    \\              " );
-		System.out.println("             [------]             " );
+		System.out.println("            |||||||||||||||          " );
+		System.out.println("           |||||||||||||||||          " );
+		System.out.println("          ||/ \\|||||||||/ \\||          " );
+		System.out.println("          ||\\ /|||||||||\\ /||          " );
+		System.out.println("           ||||||||  |||||||          " );
+		System.out.println("            |||||||  ||||||          " );
+		System.out.println("              |||||||||||        " );
+		System.out.println("                 ||||||        " );
+		System.out.println("                 ||||||        " );
+		System.out.println("            |||||||||||||||        " );
+		System.out.println("          |||||||||||||||||||        " );
+		System.out.println("         |||  |||||||||||  |||        " );
+		System.out.println("        |||  |||/    \\|||   |||        " );
+		System.out.println("       |||  ||/        \\||    |||        " );
 		//add other output
 
-		System.out.println(" \n\n\n\nHelpFul Hints" );
-		System.out.println("\\\\ draws one backslash on the screen!\n" );
-		System.out.println("\\\" draws one double quote on the screen!\n" );
-		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+		//System.out.println(" \n\n\n\nHelpFul Hints" );
+		//System.out.println("\\\\ draws one backslash on the screen!\n" );
+		//System.out.println("\\\" draws one double quote on the screen!\n" );
+		//System.out.println("\\\' draws one single quote on the screen!\n" ); 
 	}
 }

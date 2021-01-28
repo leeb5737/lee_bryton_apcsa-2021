e4a787410cc711d9170d2c8d515c5cb40978294b
leeb5737 <58750877+leeb5737@users.noreply.github.com>
2021-01-28 15:21:38 -0800

    S&S Basic Done

---
+++ Unit 01/src/StarsRunner.java
@@ -0,0 +1,23 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+	   StarsAndStripes drawer = new StarsAndStripes();
+	   
+      //call the methods needed to make the patterns on the word document
+	   drawer.printASmallBox();
+	   drawer.printTwoBlankLines();
+	   drawer.printABigBox();
+   }
+}

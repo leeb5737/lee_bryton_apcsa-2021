f40eb6da779a2bbbec4fae03718701c648f73966
leeb5737 <58750877+leeb5737@users.noreply.github.com>
2021-01-29 12:41:43 -0800

    Rename and fixed problems

--- Unit 00/src/AsciiBox.java
+++ Unit 00/src/AsciiBox.java
@@ -1,17 +1,28 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Bryton L.
+//Date - 29 January 2021
+//Class - AP Computer Science A
+//Lab  - 0a 1
 
 public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
+		System.out.println("Bryton L.(28 January 2021)" );
 		System.out.println("+++++++++++++++++++++++++ " );
-
+		System.out.println("************************* " );
+		System.out.println("######################### " );
+		System.out.println("************************* " );
+		System.out.println("---                   --- " );
+		System.out.println("(((                   ))) " );
+		System.out.println("|||       APCSA       ||| " );
+		System.out.println("(((                   ))) " );
+		System.out.println("---                   --- " );
+		System.out.println("************************* " );
+		System.out.println("######################### " );
+		System.out.println("************************* " );
+		System.out.println("+++++++++++++++++++++++++ " );
 	}
 }

ffb46eb245b21776b5d05d6fcf89c25d20dcf5d0
leeb5737 <58750877+leeb5737@users.noreply.github.com>
2021-01-28 21:15:17 -0800

    Update AsciiBox.java

--- Unit 00/src/AsciiBox.java
+++ Unit 00/src/AsciiBox.java
@@ -1,16 +1,16 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Bryton L.
+//Date - 28 January 2021
+//Class - AP Computer Science A
+//Lab  - 0a 1
 
 public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
+		System.out.println("Bryton L. \t  28 January 2021 \n\n" );
 		System.out.println("+++++++++++++++++++++++++ " );
 
 	}

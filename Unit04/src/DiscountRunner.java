01378aee318f417d931849e32bec58f25e7cd8f6
leeb5737 <58750877+leeb5737@users.noreply.github.com>
2021-02-08 12:15:18 -0800

    .

--- Unit04/src/DiscountRunner.java
+++ Unit04/src/DiscountRunner.java
@@ -21,6 +21,7 @@ public class DiscountRunner
 			out.print("Bill after Discount: " + Discount.getDiscountedBill(amt));
 			out.print("\nDo you want to continue (type E if you don't): ");
 			a = keyboard.next().charAt(0);
+		}
 			out.println("Sample Data and Outputs: ");
 			out.println("Enter the original bill amount :: 500");
 			out.println("Bill after Discount: " + Discount.getDiscountedBill(500));
@@ -32,7 +33,6 @@ public class DiscountRunner
 			out.println("Bill after Discount: " + Discount.getDiscountedBill(333.33));
 			out.println("Enter the original bill amount :: 95874.2154");
 			out.println("Bill after Discount: " + Discount.getDiscountedBill(95874.2154));
-		}
 
 	}
 }
